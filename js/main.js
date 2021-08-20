de55ccc3f71a1fa5988dae1b77f6d00114970ccc
Jonat J Mathew <johnatj33@gmail.com>
2021-08-20 15:22:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+/*==================== SLIDESHOW ====================*/
+
 var swiper = new Swiper(".home__container", {
   cssMode: true,
   loop: true,
@@ -11,3 +14,56 @@ var swiper = new Swiper(".home__container", {
     clickable: true,
   },
 });
+
+
+var swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+     navClose = document.getElementById('nav-close')
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle) {
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose) {
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Em An
+// 10/10/2016
+// https://codepen.io/anon/pen/qaoxyA  
+// https://codepen.io/Em-An/pen/LRdjwp  
